facd31ede73d8c6f5b7e263b23e9ab2fe8dbdc83
Amrakh9 <emrahamiraslanov@gmail.com>
2024-05-30 14:31:45 +0400

    Initial Commit

---
+++ findKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  findKenny
+//
+//  Created by Amrah on 30.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
